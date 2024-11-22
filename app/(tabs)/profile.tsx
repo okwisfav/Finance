d96fbad115a974649ad5ddb6ccee7b198cd97d4b
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-22 15:04:50 +0100

    .....

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NetflixIcon } from '@/constants/Icons'
 
 const profile = () => {
   return (
     <View>
-      <Text>profile</Text>
+      <Text>profile</Text> 
+      
     </View>
   )
 }
