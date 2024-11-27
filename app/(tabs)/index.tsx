1954c3a429fdc47e2a8f0ef66a19bc7dc4a1db86
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-27 11:49:47 +0100

    ....

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Colors from '@/constants/Colors'; // Ensure the path to Colors is correct or define it inline
 import { Stack } from 'expo-router';
 import Header from '@/components/Header';
@@ -13,8 +13,10 @@ const Pages = () => {
           header: () => <Header />, // Replace with your custom header if needed
         }}
       />
-      <View style={styles.container}>
-        <Text style={styles.text}>Pages</Text>
+      <View style={[styles.container, {paddingTop:40}]}>
+         <ScrollView showsVerticalScrollIndicator={false}>
+              <Text style={{color:Colors.white}}>My Expenses</Text>
+         </ScrollView>
       </View>
     </>
   );
