4fc7d33fb5f26da659e7b21a9be03ceefe917982
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-27 09:44:33 +0100

    ...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NetflixIcon } from '@/constants/Icons'
 
 const Pages = () => {
   return (
     <View>
       <Text>Pages</Text>
+      {/* <NetflixIcon width={22} height={22} color={'#000'} /> */}
     </View>
   )
 }
