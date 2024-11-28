71d6f194d03e75e7424deaa027705f09a76acf16
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-28 16:53:52 +0100

    ...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,12 +15,17 @@ const Pages = () => {
       />
       <View style={[styles.container, {paddingTop:50}]}>
          <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{flexDirection: 'row', justifyContent:'space-between', alignItems:'center'}}>
               <View style={{gap: 10}}>
                 <Text style={{color:Colors.white, fontSize: 16}}>
                    My <Text style={{fontWeight:700}}>Expenses</Text> 
                 </Text>
                 <Text style={{color:Colors.white, fontSize:36, fontWeight:700}}>$2000.<Text style={{fontSize:22, fontWeight:400 }}>00</Text></Text>
               </View>
+              <View>
+                  <Text style={{color:Colors.white}}>Pie Chart</Text>
+              </View>
+            </View>
          </ScrollView>
       </View>
     </>
