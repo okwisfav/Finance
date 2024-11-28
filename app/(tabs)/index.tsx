4f5de3798dec27303c395a56e9f50eac9e47e717
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-11-28 17:12:03 +0100

    ....

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,29 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Colors from '@/constants/Colors'; 
 import { Stack } from 'expo-router';
 import Header from '@/components/Header';
- 
+import { PieChart } from "react-native-gifted-charts";
 
 const Pages = () => {
+  const pieData = [
+    {
+      value: 47,
+      color: Colors.tintColor,
+      focused: true,
+      text: "47%",
+    },
+    {
+      value: 40,
+      color: Colors.blue,
+      text: "40%",
+    },
+    {
+      value: 16,
+      color: Colors.white,
+      text: "16%",
+    },
+    { value: 3, color: "#FFA5BA", gradientCenterColor: "#FF7F97", text: "3%" },
+  ];
+
   return (
     <>
       <Stack.Screen
@@ -22,8 +42,36 @@ const Pages = () => {
                 </Text>
                 <Text style={{color:Colors.white, fontSize:36, fontWeight:700}}>$2000.<Text style={{fontSize:22, fontWeight:400 }}>00</Text></Text>
               </View>
-              <View>
-                  <Text style={{color:Colors.white}}>Pie Chart</Text>
+             
+              <View style={{paddingVertical:20,alignItems:'center'}}>
+              <PieChart
+                data={pieData}
+                donut
+                showGradient
+                sectionAutoFocus
+                focusOnPress
+                semiCircle
+                radius={70}
+                innerRadius={55}
+                innerCircleColor={Colors.black}
+                centerLabelComponent={() => {
+                  return (
+                    <View
+                      style={{ justifyContent: "center", alignItems: "center" }}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 22,
+                          color: "white",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        47%
+                      </Text>
+                    </View>
+                  );
+                }}
+              />
               </View>
             </View>
          </ScrollView>
---
+++ components/ExpenseBlock.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ExpenseBlock = () => {
+  return (
+    <View>
+      <Text>ExpenseBlock</Text>
+    </View>
+  )
+}
+
+export default ExpenseBlock
+
+const styles = StyleSheet.create({})
