afc58324cf0475c86efd138a1df9b7440ad25071
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-12-02 13:20:56 +0100

    ...

---
+++ components/SpendingBlock.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SpendingBlock = () => {
+  return (
+    <View>
+      <Text>SpendingBlock</Text>
+    </View>
+  )
+}
+
+export default SpendingBlock
