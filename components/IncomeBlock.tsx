a85ce25ad5cb96febfef3bbf60c858a7f73109f3
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-12-02 12:46:39 +0100

    ....

--- components/IncomeBlock.tsx
+++ components/IncomeBlock.tsx
@@ -2,18 +2,30 @@ import { FlatList, ListRenderItem, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Colors from '@/constants/Colors'
 import { IncomeType } from '@/types'
+import { DollarIcon } from '@/constants/Icons'
 
 const IncomeBlock = ({ incomeList }: { incomeList: IncomeType[] }) => {
    const renderItem:ListRenderItem<IncomeType> = ({item}) => {
      return(
-       <View>
+       <View style={{
+        backgroundColor: Colors.grey,
+        padding: 20,
+        borderRadius: 20,
+        marginRight: 15, 
+        width: 150, 
+        gap:10
+        }}
+        >
+           <View>
+               <DollarIcon width={22} height={22} color={Colors.white} />
+           </View>
            <Text style={{color:Colors.white}}>{item.name}</Text>
        </View>
      );
    } 
   return (
     <View>
-      <Text style={{color:Colors.white,fontSize: 16,}}>IncomeBlock</Text>
+      <Text style={{color:Colors.white,fontSize: 16, marginBottom: 20}}>IncomeBlock</Text>
       <FlatList data={incomeList} renderItem={renderItem} horizontal showsHorizontalScrollIndicator={false}/>
     </View>
   )
