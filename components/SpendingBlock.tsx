f0c9caf4440733978f80993b5ccaa399d268372e
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-12-02 16:11:26 +0100

    ....

--- components/SpendingBlock.tsx
+++ components/SpendingBlock.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Colors from '@/constants/Colors'
+import { SpendingType } from '@/types'
 
 const SpendingBlock = ({ spendingList }: { spendingList: SpendingType[] }) => {
   return (
