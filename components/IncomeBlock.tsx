5df6d74fb0bbf72f09859975624f35188ef0ed4c
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2024-12-02 13:13:04 +0100

    ....

--- components/IncomeBlock.tsx
+++ components/IncomeBlock.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { FlatList, ListRenderItem, Text, TouchableOpacity, View } from 'react-native';
-import Colors from '@/constants/Colors';
-import { IncomeType } from '@/types';
-import { DollarIcon } from '@/constants/Icons';
-import { Feather } from '@expo/vector-icons';
+import {
+  FlatList,
+  ListRenderItem,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+import { IncomeType } from "@/types";
+import { DollarIcon, WalletAddMoneyIcon, WalletCardIcon } from "@/constants/Icons";
+import { Feather } from "@expo/vector-icons";
 
 const IncomeBlock = ({ incomeList }: { incomeList: IncomeType[] }) => {
   const renderItem: ListRenderItem<IncomeType> = ({ item }) => {
+    let icon = <DollarIcon width={22} height={22} color={Colors.white} />;
+    if ( item.name == 'Freelancing' ) {
+      icon = <WalletCardIcon width={22} height={22} color={Colors.white} />
+    } else if (item.name == 'Interest') {
+      icon = <WalletAddMoneyIcon width={22} height={22} color={Colors.white} />
+    }
+
+    let amount = item.amount.split(".");
     return (
       <View
         style={{
@@ -18,32 +33,33 @@ const IncomeBlock = ({ incomeList }: { incomeList: IncomeType[] }) => {
           gap: 10,
         }}
       >
-        {/* Header Row */}
         <View
           style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
           <View
             style={{
-              borderColor: '#666',
+              borderColor: "#666",
               borderWidth: 1,
               borderRadius: 50,
               padding: 5,
-              alignSelf: 'flex-start',
+              alignSelf: "flex-start",
             }}
           >
-            <DollarIcon width={22} height={22} color={Colors.white} />
+            {icon}
           </View>
           <TouchableOpacity onPress={() => {}}>
             <Feather name="more-horizontal" size={20} color={Colors.white} />
           </TouchableOpacity>
         </View>
-        {/* Item Name */}
         <Text style={{ color: Colors.white }}>{item.name}</Text>
-        <Text style={{ color: Colors.white }}>{item.amount}</Text>
+        <Text style={{ color: Colors.white, fontSize: 18, fontWeight: "600" }}>
+          ${amount[0]}.
+          <Text style={{ fontSize: 12, fontWeight: "400" }}>{amount[1]}</Text>
+        </Text>
       </View>
     );
   };
@@ -51,17 +67,18 @@ const IncomeBlock = ({ incomeList }: { incomeList: IncomeType[] }) => {
   return (
     <View>
       <Text style={{ color: Colors.white, fontSize: 16, marginBottom: 20 }}>
-        IncomeBlock
+        My <Text style={{ fontWeight: "700" }}>Income</Text>
       </Text>
       <FlatList
         data={incomeList}
         renderItem={renderItem}
         horizontal
         showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.id.toString()} // Assuming `id` is unique in `IncomeType`
       />
     </View>
   );
 };
 
 export default IncomeBlock;
+
+const styles = StyleSheet.create({});
